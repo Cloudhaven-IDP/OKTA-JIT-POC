3e2cb80dc32e65e8896d7e14083b33c2ffa44dea
Afolabi Fajobi <afolabi.fajobi1@yahoo.com>
2026-05-26 13:49:04 -0400

    fix okta -> aws saml
    Signed-off-by: Afolabi Fajobi <afolabi.fajobi1@yahoo.com>

--- infra/okta/apps.tf
+++ infra/okta/apps.tf
@@ -1,16 +1,9 @@
-resource "okta_app_saml" "aws_idc" {
-  label             = "AWS IAM Identity Center (JIT POC)"
-  preconfigured_app = "amazon_aws"
-
-  attribute_statements {
-    type         = "GROUP"
-    name         = "groups"
-    filter_type  = "REGEX"
-    filter_value = "(jit-requesters|team-.*)"
-  }
+resource "okta_app_bookmark" "aws_access_portal" {
+  label = "AWS Access Portal"
+  url   = "https://d-90660ebbc1.awsapps.com/start"
 }
 
-resource "okta_app_group_assignment" "jit_requesters_to_aws_idc" {
-  app_id   = okta_app_saml.aws_idc.id
+resource "okta_app_group_assignment" "jit_requesters_to_aws" {
+  app_id   = okta_app_bookmark.aws_access_portal.id
   group_id = okta_group.this["jit-requesters"].id
 }
